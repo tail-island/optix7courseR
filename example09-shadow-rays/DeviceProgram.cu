9ad258331bd4c4f2eb96c6c6b054188c87fd74ad
OJIMA Ryoji <rojima1@gmail.com>
2022-01-27 14:26:32 +0900

    refactored.

--- example09-shadow-rays/DeviceProgram.cu
+++ example09-shadow-rays/DeviceProgram.cu
@@ -40,7 +40,7 @@ extern "C" __global__ void __raygen__renderFrame() {
 
   // カメラの情報を取得します。
 
-  auto &origin = *reinterpret_cast<Eigen::Vector3f *>(&optixLaunchParams.camera.origin);
+  auto &origin = *reinterpret_cast<Eigen::Vector3f *>(&optixLaunchParams.camera.origin); // optixTraceの都合で、const autoに出来ない……。
 
   const auto &u = *reinterpret_cast<Eigen::Vector3f *>(&optixLaunchParams.camera.u);
   const auto &v = *reinterpret_cast<Eigen::Vector3f *>(&optixLaunchParams.camera.v);
@@ -48,12 +48,12 @@ extern "C" __global__ void __raygen__renderFrame() {
 
   // レイの方向を計算します。
 
-  auto direction = ((static_cast<float>(x) / optixGetLaunchDimensions().x * 2 - 1) * u + (static_cast<float>(y) / optixGetLaunchDimensions().y * 2 - 1) * v + w).normalized();
+  auto direction = ((static_cast<float>(x) / optixGetLaunchDimensions().x * 2 - 1) * u + (static_cast<float>(y) / optixGetLaunchDimensions().y * 2 - 1) * v + w).normalized(); // optixTraceの都合で、const autoに出来ない……。
 
   // ピクセルの色を表現する変数を用意します。この値をoptixTraceして設定します。
 
   auto color = Eigen::Vector3f{0};
-  auto [payloadParam0, payloadParam1] = getPayloadParams(&color);
+  auto [payloadParam0, payloadParam1] = getPayloadParams(&color); // optixTraceの都合で、const autoに出来ない……。
 
   // optixTraceして、レイをトレースします。
 
@@ -77,7 +77,7 @@ extern "C" __global__ void __raygen__renderFrame() {
   optixLaunchParams.imageBuffer[x + y * optixGetLaunchDimensions().x] = float3{color.x(), color.y(), color.z()};
 }
 
-// 物体に光が衝突した場合の処理です。衝突判定は自動でやってくれます。
+// 物体にレイが衝突した場合の処理です。衝突判定は自動でやってくれます。
 
 extern "C" __global__ void __closesthit__radiance() {
   const auto &triangleMeshes = reinterpret_cast<HitgroupData *>(optixGetSbtDataPointer())->triangleMeshes;
@@ -122,20 +122,20 @@ extern "C" __global__ void __closesthit__radiance() {
 
   // レイが衝突した場所（から、同じポリゴンに再衝突しないように法線方向に少しずらした場所）を取得します。
 
-  auto hitPosition = [&] {
+  auto hitPosition = [&] { // optixTraceの都合で、const autoに出来ない……。
     return static_cast<Eigen::Vector3f>((1 - u - v) * triangleMeshes.vertices[index.x()] + u * triangleMeshes.vertices[index.y()] + v * triangleMeshes.vertices[index.z()] + normal * 1e-3f); // Eigenは必要になるまで計算を遅らせるので、static_castしないとoptixTraceで計算途中の値をreinterpret_castされちゃう……。
   }();
 
   // レイが衝突した場所から光源への方向を取得します。
 
-  auto toLight = [&] {
+  auto toLight = [&] { // optixTraceの都合で、const autoに出来ない……。
     return static_cast<Eigen::Vector3f>(*reinterpret_cast<Eigen::Vector3f *>(&optixLaunchParams.lightPosition) - hitPosition);
   }();
 
   // レイが衝突した場所から光源が見えるかを表現する変数を用意します。この値をoptixTraceして設定します。
 
   auto isLightVisible = false;
-  auto [payloadParam0, payloadParam1] = getPayloadParams(&isLightVisible);
+  auto [payloadParam0, payloadParam1] = getPayloadParams(&isLightVisible); // optixTraceの都合で、const autoに出来ない……。
 
   // 影を生成するためのレイを使用して、optixTraceします。
 
@@ -165,7 +165,7 @@ extern "C" __global__ void __anyhit__radiance() {
   ; // このコースでは、なにもしません。
 }
 
-// トレースした光が物体に衝突しなかった場合の処理です。
+// レイが物体に衝突しなかった場合の処理です。
 
 extern "C" __global__ void __miss__radiance() {
   *reinterpret_cast<Eigen::Vector3f *>(getPayloadPointer()) = Eigen::Vector3f{1, 1, 1}; // とりあえず、背景は真っ白にします。
